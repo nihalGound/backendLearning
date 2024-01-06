9f3fe4c29179084787650a2a521056e9858c317f
nihalGound <nsnihalgound123@gmail.com>
2024-01-06 21:40:45 +0530

    fix auth middleware get only accesstoken from req.body

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import { User } from "../models/User.model.js";
 
 const auth = async (req, _,next)=>{
     try {
-        const token = req.cookies?.accessToken || req.cookies?.refreshToken || req.header("Authorization")?.replace("Bearer ","");
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
         if(!token){
             throw new apiError(401,"Unauthorized user");
         }
